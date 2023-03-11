7523d84c9736908183e7dd8a4055730e3f74a4aa
Vojtěch Riedl <vojtariedl66@gmail.com>
2023-03-11 15:36:55 +0100

    fix

--- Assets/Scripts/PlanetGeneration.cs
+++ Assets/Scripts/PlanetGeneration.cs
@@ -5,37 +5,30 @@ using UnityEngine;
 public class PlanetGeneration : MonoBehaviour
 {
     [SerializeField]
-    List<GameObject> planets = new List<GameObject>();
-    [SerializeField]
     private Player player;
+
+    [SerializeField]
+    private PlanetManager planetManager;
     private bool isGenerated = false;
 
     public bool IsGenerated { get => isGenerated; set => isGenerated = value; }
 
-    void Awake()
+    void Start()
     {
-        player.ActualPlanet = SpawnPlanet();
+        player.ActualPlanet = SpawnPlanet(); 
     }
 
     void Update()
     {
         if (!isGenerated)
         {
-            player.ActualPlanet = SpawnPlanet();
+            //player.ActualPlanet = SpawnPlanet();
         }
-
-    }
-
-
-    public Planet GetRandomPlanet()
-    {
-        return planets[UnityEngine.Random.Range(0, planets.Count)]
-                .GetComponent<Planet>();
     }
 
     private Planet SpawnPlanet()
     {
-        var planet = GetRandomPlanet();
+        var planet = planetManager.spawnRandomPlanet();
         isGenerated = true;
         planet = planet.GeneratePlanet();
         player.gameObject.transform.position = new Vector3(planet.SpawnPoint.gameObject.transform.position.x, planet.SpawnPoint.gameObject.transform.position.y, player.gameObject.transform.position.z);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,26 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-<<<<<<< Updated upstream
     private InputManager InputManager;
     [SerializeField]
     private float speed = 15f;
     [SerializeField]
-    private float jumpForce = 1f;
-=======
     private GameObject Upgrades;
 
-
-    // Start is called before the first frame update
->>>>>>> Stashed changes
 
     [SerializeField]
     private Rigidbody playerRigid;
 
     private Planet actualPlanet;
 
-    //private CharacterController controller;
-    private bool isJumping = false;
     public Planet ActualPlanet { get => actualPlanet; set => actualPlanet = value; }
     public Rigidbody PlayerRigid { get => playerRigid; }
 
@@ -35,53 +27,17 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
-<<<<<<< Updated upstream
-        movement();
     }
 
-    public void movement()
-    {
-        /*Vector2 vector = InputManager.GetVector2();
-        if (Input.GetKey(KeyCode.W))
-        {
-            vector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            vector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            vector.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            vector.x += 1;
-        }
-        if (Input.GetKey(KeyCode.Space) && isJumping == false)
-        {
-            //playerRigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isJumping = true;
-        }
-
-        vector = vector.normalized;
-
-        transform.position += new Vector3(vector.x,0f, vector.y) * speed * Time.deltaTime;*/
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-        Debug.DrawRay(moveDirection,Vector3.forward * 5, Color.red);
-
-    }
+   
 
     private void FixedUpdate()
     {
-        playerRigid.MovePosition(playerRigid.position + transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
-=======
         if(Input.GetKeyDown("u"))
         {
             Upgrades.SetActive(!Upgrades.active);
 
         }
         
->>>>>>> Stashed changes
     }
 }
