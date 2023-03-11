3520cbe3c884ca697885bc0b34799662f7f547c8
Matěj Bezděka <79998445+MatejBezdeka@users.noreply.github.com>
2023-03-11 23:06:50 +0100

    Floors

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
     public void Start() {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("MainScene");
     }
 
     public void Exit() {
